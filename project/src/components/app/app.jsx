e500139a55b61f4b68c2c12ad852cf1e3d18f828
Svetlana Oshepkova <lana_nnov@mail.ru>
2021-06-04 11:13:13 +0300

    Создаю новые компоненты

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import MainPage from '../main-page/main-page';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const { placesCount } = props;
+
+  return (
+    <MainPage placesCount={placesCount} />
+  );
 }
+
+App.propTypes = {
+  placesCount: PropTypes.number.isRequired,
+};
 
 export default App;
