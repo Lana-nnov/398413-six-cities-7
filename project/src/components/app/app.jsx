a280db85130698349ea26e4b47d6cae624adc69e
Svetlana Oshepkova <lana_nnov@mail.ru>
2021-06-07 18:00:57 +0300

    Добавляю маршрутизацию

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
-import MainPage from '../main-page/main-page';
 import PropTypes from 'prop-types';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { AppRoute } from '../../const';
+import MainPage from '../main-page/main-page';
+import Login from '../login/login';
+import Favorite from '../favorite/favorite';
+import Room from '../room/room';
+import NotFoundPage from '../not-found-page/not-found-page';
 
 function App(props) {
   const { placesCount } = props;
 
   return (
-    <MainPage placesCount={placesCount} />
+    < BrowserRouter >
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <MainPage placesCount={placesCount} />
+        </Route>
+        <Route exact path={AppRoute.LOGIN}>
+          <Login />
+        </Route>
+        <Route exact path={AppRoute.FAVORITE}>
+          <Favorite />
+        </Route>
+        <Route exact path={AppRoute.ROOM}>
+          <Room />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
+      </Switch>
+    </BrowserRouter >
   );
 }
 
---
+++ project/src/const.js
@@ -0,0 +1,6 @@
+export const AppRoute = {
+  LOGIN: '/login',
+  FAVORITE: '/favorite',
+  ROOM: '/room/:id?',
+  ROOT: '/',
+};
